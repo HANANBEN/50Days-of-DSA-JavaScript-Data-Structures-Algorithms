48c4fc228e4707e4e4cbb4d1414ead6f7844c22c
HANANBEN <hbenazize@gmail.com>
2025-08-22 14:56:47 +0200

    Day 2 : Recursion

---
+++ Recursion/Recursion_Leap_of_faith.js
@@ -0,0 +1,11 @@
+
+
+/**
+ * recursion leap of faith : 
+ * 
+ * 1. understand the problem  ==> print 321123
+ * 2.Identify subproblem  ==> print 2112
+ * 3.trust // fail  ===>  you dont need to unroll the recursion
+ * 4. Link 1 and 2  ==> 321123
+ * 4.Base conditon    ==> for 0 , return 
+ */
