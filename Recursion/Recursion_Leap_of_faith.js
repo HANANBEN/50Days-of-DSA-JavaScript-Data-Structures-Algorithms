59d216eb7b8b31112d2d44d45f88d711be3b880f
HANANBEN <hbenazize@gmail.com>
2025-08-24 13:25:42 +0200

    Day 2 :recursion leap of faith concept

--- Recursion/Recursion_Leap_of_faith.js
+++ Recursion/Recursion_Leap_of_faith.js
@@ -9,3 +9,42 @@
  * 4. Link 1 and 2  ==> 321123
  * 4.Base conditon    ==> for 0 , return 
  */
+
+
+/**
+ * let's explain this algorithm ,, it's quite interessting 
+ * 
+ * /**
+ * Recursion "leap of faith" to print mirror numbers (ex: 321123 for n=3)
+ * 
+ * 1. Base case: when n == 0, return (stop recursion).
+ * 2. Print n before the recursive call (on the way down).
+ * 3. Make the recursive call with n-1.
+ * 4. The code after recursion does not run immediately.
+ *    It waits until the *last recursive call returns*.
+ * 5. Once the base case returns, recursion "calls back":
+ *    it starts executing the code after recursion in each
+ *    pending function call (on the way up the stack).
+ * 6. That’s why we see the numbers printed again in reverse
+ *    order → 321123.
+ 
+ */
+
+function testRecursion(n){
+     
+
+
+     if( n ==0) return 0 ;
+       
+
+      console.log(n);
+    testRecursion(n-1); 
+      console.log(n);
+
+
+
+    }
+
+
+
+testRecursion(5);
